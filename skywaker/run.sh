a69ae1767db25306b51d0c80074c7fc8a426e10f
David Wagner <f2@hudecity.de>
2018-03-26 19:29:10 +0200

    first version

--- skywaker/run.sh
+++ skywaker/run.sh
@@ -5,6 +5,4 @@ CONFIG_PATH=/data/options.json
 PORT=$(jq --raw-output ".port" $CONFIG_PATH)
 SKYIP=$(jq --raw-output ".skyip" $CONFIG_PATH)
 
-sky-hassio-waker --port=${PORT} --skyip=${SKYIP} --host=0.0.0.0
-
-
+sky-hassio-waker --port=3032 --skyip=192.168.1.180 --host=0.0.0.0
